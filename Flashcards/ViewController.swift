634bf854e5228e9f4ab1981b870289f6d0039c96
ChrisPCodes <61615462+ChrisPCodes@users.noreply.github.com>
2021-03-06 17:42:22 -0800

    Gave flashcards rounded corners and a shadow to stand out more. Also changed answer flashcard color. Added multiple choice selection.

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -11,16 +11,70 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var card: UIView!
+        
+    @IBOutlet weak var btnOptionOne: UIButton!
+    @IBOutlet weak var btnOptionTwo: UIButton!
+    @IBOutlet weak var btnOptionThree: UIButton!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Rounded flashcard corners must clip to bounds
+        frontLabel.layer.cornerRadius = 20.0
+        backLabel.layer.cornerRadius = 20.0
+        frontLabel.clipsToBounds = true
+        backLabel.clipsToBounds = true
+        
+        // Shadow
+        card.layer.cornerRadius = 20.0
+        card.layer.shadowRadius = 18.0
+        card.layer.shadowOpacity = 2.0
+        
+        // Button One
+        btnOptionOne.layer.cornerRadius = 20.0
+        btnOptionOne.clipsToBounds = true
+        btnOptionOne.layer.borderWidth = 3.0
+        btnOptionOne.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        
+        // Button Two
+        btnOptionTwo.layer.cornerRadius = 20.0
+        btnOptionTwo.clipsToBounds = true
+        btnOptionTwo.layer.borderWidth = 3.0
+        btnOptionTwo.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        
+        // Button Three
+        btnOptionThree.layer.cornerRadius = 20.0
+        btnOptionThree.clipsToBounds = true
+        btnOptionThree.layer.borderWidth = 3.0
+        btnOptionThree.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
     }
-
-    @IBAction func didTapOnFlashcard(_ sender: Any) {
+    
+    @IBAction func didTapOptionOne(_ sender: Any) {
+        btnOptionOne.isHidden = true
+    }
+    
+    @IBAction func didTapOptionTwo(_ sender: Any) {
         frontLabel.isHidden = true
     }
     
+    @IBAction func didTapOptionThree(_ sender: Any) {
+        btnOptionThree.isHidden = true
+    }
+    
+    
+    
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        
+        // If front label is hidden unhide otherwise hide it
+        if frontLabel.isHidden {
+            frontLabel.isHidden = false
+        }
+        else {
+            frontLabel.isHidden = true
+        }
+    }
 }
 
--- Readme.md
+++ Readme.md
@@ -1,7 +1,5 @@
 ## Flashcards app
 
-📝 `NOTE:` Paste the README templates for each subsequent lab here at the top, (i.e. lab 2, 3, 4). This will show a history of your development process including which users stories you completed and how your app looked and functioned at each step.
-
 ## Lab 1
 
 ### App Description
@@ -18,6 +16,6 @@ This is a flashcard app which currently allows the user to learn how hot the sun
 - [X] Build in logic to show the answer side when the card is tapped
 - [X] Push code to GitHub
 ## Optional
-- [ ] Toggle the flashcard between the question side and the answer side
-- [ ] Style the question and answer side of the card to better distinguish between the two sides
-- [ ] Add selectable multiple choice answers beneath the card
+- [X] Toggle the flashcard between the question side and the answer side
+- [X] Style the question and answer side of the card to better distinguish between the two sides
+- [X] Add selectable multiple choice answers beneath the card
